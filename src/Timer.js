12dd89dc7271986452078c17490d7f239d897207
Jack Wiencek <wiencek.16@osu.edu>
2024-11-28 13:34:32 -0500

    adding timer

---
+++ src/Timer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useState } from 'react';
+function Timer() {
+  const [minute, updateMinute] = useState(0);
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Timer;
